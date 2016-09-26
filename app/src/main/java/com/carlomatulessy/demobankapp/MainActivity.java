fc989602ab86c6364a2ae42dda8661913fe57036
carlo.matulessy <carlo.matulessy@capgemini.com>
2016-09-26 20:26:10 +0200

    Commit with alert dialog function in it. Delete function works, transfer function needs to update.

--- app/src/main/java/com/carlomatulessy/demobankapp/MainActivity.java
+++ app/src/main/java/com/carlomatulessy/demobankapp/MainActivity.java
@@ -1,19 +1,25 @@
 package com.carlomatulessy.demobankapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView mutationList;
     public static TextView accountAmount;
+    private MutationListAdapter mlAdapt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +48,57 @@ public class MainActivity extends AppCompatActivity {
 
         Drawable userIcon = ResourcesCompat.getDrawable(getResources(), R.drawable.carlo, null);
         accountIcon.setImageDrawable(userIcon);
-        
-        mutationList.setAdapter(new MutationListAdapter(this, R.layout.dashboard_mutationlist, DataBuilder.getAccounts()));
+
+        mlAdapt = new MutationListAdapter(this, R.layout.dashboard_mutationlist, DataBuilder.getAccounts());
+        mutationList.setAdapter(mlAdapt);
+
+        mutationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showAccountDetails(position);
+            }
+        });
+    }
+
+    private void showAccountDetails(int position) {
+        final Account selectedAccount = DataBuilder.getAccounts().get(position);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle("Mutation info");
+        alertDialogBuilder
+                .setMessage(selectedAccount.getName() + " - " + "€ "+ String.format("%.2f",selectedAccount.getAmount()))
+                .setCancelable(false)
+                .setPositiveButton("Transfer", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        transferDialogTransfer();
+                    }
+                })
+                .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        transferDialogDelete(selectedAccount);
+                    }
+                });
+
+        AlertDialog transferDialog = alertDialogBuilder.create();
+        transferDialog.show();
+    }
+
+    private void transferDialogTransfer() {
+
+    }
+
+    private void transferDialogDelete(Account accountDelete) {
+        for(Iterator<Account> iteratorAccounts = DataBuilder.getAccounts().iterator(); iteratorAccounts.hasNext(); ) {
+            Account account = iteratorAccounts.next();
+            if (account.getName() == accountDelete.getName() && account.getAmount() == accountDelete.getAmount() && account.getIban() == accountDelete.getIban()) {
+                iteratorAccounts.remove();
+                break;
+            }
+        }
+
+        mlAdapt.notifyDataSetChanged();
     }
 
     public void startTransfer(View view) {
